a8f8ec5523f9cc4332d7fb9993ec6cf2353b9864
ChrisK29 <Chriskappeler2@gmail.com>
2022-08-23 10:57:30 -0400

    frontend added

---
+++ frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+
+	constructor(
+		public description: string,
+		public image: string,
+		public name: string,
+		public price: number,
+		public quantity: number,
+		public weight: number,
+	) {
+
+	}
+}
